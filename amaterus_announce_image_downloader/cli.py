7c045fa38f8cd71d0ed519b227b475bf1dcb299c
aoirint <aoirint@gmail.com>
2023-12-09 13:12:41 +0900

    add youtube_live_thumbnail_image subcommand

--- amaterus_announce_image_downloader/cli.py
+++ amaterus_announce_image_downloader/cli.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from . import __VERSION__ as APP_VERSION
 from .app_config import load_app_config_from_env
 from .twitter_tweet_image_cli import add_twitter_tweet_image_arguments
+from .youtube_live_thumbnail_image_cli import add_youtube_live_thumbnail_image_arguments
 
 
 def main() -> None:
@@ -26,6 +27,14 @@ def main() -> None:
     subparser_twitter_tweet_image = subparsers.add_parser("twitter_tweet_image")
     add_twitter_tweet_image_arguments(
         parser=subparser_twitter_tweet_image,
+        app_config=app_config,
+    )
+
+    subparser_youtube_live_thumbnail_image = subparsers.add_parser(
+        "youtube_live_thumbnail_image"
+    )
+    add_youtube_live_thumbnail_image_arguments(
+        parser=subparser_youtube_live_thumbnail_image,
         app_config=app_config,
     )
 
